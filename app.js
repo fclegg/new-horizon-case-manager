3a3f46eb3be861ebf7bf730d4d59289c9047167e
fclegg <fclegg.newhorizon@gmail.com>
2026-08-21 00:14:53 -0500

    Update app.js
    temp

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const testButton = document.getElementById("testButton");
-const status = document.getElementById("status");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+import { 
+    getAuth 
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { 
+    firebaseConfig 
+} from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+console.log("Firebase connected successfully.");
 testButton.addEventListener("click", function () {
     status.textContent = "New Horizon Case Manager is working.";
 });
