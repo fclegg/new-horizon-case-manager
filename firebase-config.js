9d7b75ba9244abf0b8b93f2178c22e85260758e1
fclegg <fclegg.newhorizon@gmail.com>
2026-08-21 00:12:33 -0500

    Create firebase-config.js
    v1

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_a3QoU4JfRqtp-dfeEnDgQjmyNC59rRY",
+  authDomain: "new-horizon-case-manager.firebaseapp.com",
+  projectId: "new-horizon-case-manager",
+  storageBucket: "new-horizon-case-manager.firebasestorage.app",
+  messagingSenderId: "457648232954",
+  appId: "1:457648232954:web:8e3348489d33291c1b6c43",
+  measurementId: "G-V89EHGZKQY"
+};
