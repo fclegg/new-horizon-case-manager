8ebea53b9fb13d26279c5dc2fa61244c52757b2a
fclegg <fclegg.newhorizon@gmail.com>
2026-08-21 00:01:18 -0500

    Update app.js
    v1

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const testButton = document.getElementById("testButton");
+const status = document.getElementById("status");
 
+testButton.addEventListener("click", function () {
+    status.textContent = "New Horizon Case Manager is working.";
+});
